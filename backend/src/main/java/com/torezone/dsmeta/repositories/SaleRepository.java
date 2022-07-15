08e179a36861f448b5e8b78f9404a25fae7bfbc0
Jair Torezone <jairtorezone@gmail.com>
2022-07-14 21:29:37 -0300

    API test

---
+++ backend/src/main/java/com/torezone/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.torezone.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.torezone.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
